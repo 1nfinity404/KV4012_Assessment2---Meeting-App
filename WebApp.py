c3084f5ad7e40b8fec2d2d10bdd505b9eb4604ba
1nfinity404 <jialehuang146@gmail.com>
2025-03-21 14:19:54 +0000

    Add log in account form

--- WebApp.py
+++ WebApp.py
@@ -4,3 +4,8 @@ from pywebio import start_server
 from datetime import datetime
 
 put_html("<h1 class = text-center>Log In</h1>")
+
+log_account = input_group('',[
+    input("Enter your username: ", name="name", required=True),
+    input("Enter your password: ", name="password", required=True, type=PASSWORD)
+])
